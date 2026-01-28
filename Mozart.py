d2616108169915730d97e728e0cbc51ecbdf4afd
RenX86 <134190723+RenX86@users.noreply.github.com>
2026-01-28 23:38:49 +0530

    Repo Initialised

---
+++ Mozart.py
@@ -0,0 +1,31 @@
+import os
+import discord
+from discord.ext import commands
+from discord import app_commands
+from dotenv import load_dotenv
+
+
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    await bot.tree.sync()
+    print(f"{bot.user} is online!")
+    
+@bot.event
+async def on_message(msg):
+    if msg.author.id != bot.user.id:
+        await msg.channel.send(f"Interesting message, {msg.author.mention}")
+        
+@bot.tree.command(name="greet", description="sends a greeting to the user")
+async def greet(interaction: discord.Interaction):
+    username = interaction.user.mention
+    await interaction.response.send_message(f"Hello There, {username}")
+    
+bot.run(TOKEN)
